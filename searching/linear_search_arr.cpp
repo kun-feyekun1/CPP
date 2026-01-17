52dcc2b1d9ae9bbccdb53c3f82281c5ddd3868a1
kun-feyekun1 <mohammed.ugr-2340-16@aau.edu.et>
2026-01-17 06:12:15 -0200

    searching modfied

--- searching/linear_search_arr.cpp
+++ searching/linear_search_arr.cpp
@@ -1,20 +1,31 @@
 #include <iostream>
 using namespace std;
 
-int linearSearchArray(int arr[], int n, int key){
-    for(int i=0;i<n;i++) if(arr[i]==key) return i;
+int linearSearchArray(int arr[], int n, int key)
+{
+    for (int i = 0; i < n; i++)
+        if (arr[i] == key)
+            return i;
     return -1;
 }
 
-int main(){
-    int n; cout << "Enter array size: "; cin >> n;
+int main()
+{
+    int n;
+    cout << "Enter array size: ";
+    cin >> n;
     int arr[n];
     cout << "Enter array elements:\n";
-    for(int i=0;i<n;i++) cin >> arr[i];
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
 
-    int key; cout << "Enter key to search: "; cin >> key;
+    int key;
+    cout << "Enter key to search: ";
+    cin >> key;
 
     int idx = linearSearchArray(arr, n, key);
-    if(idx>=0) cout << "Found at index: " << idx << "\n";
-    else cout << "Not found\n";
+    if (idx >= 0)
+        cout << "Found at index: " << idx << "\n";
+    else
+        cout << "Not found\n";
 }
